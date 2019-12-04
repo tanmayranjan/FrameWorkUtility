72712251c0570e2c5d7b633ca369e4fd24f8cfc4
Tanmay Ranjan <Tanmay.Ranjan@in.niit.com>
2019-12-04 10:04:59 +0530

    added try catch in controller

--- app/controllers/FrameworkController.java
+++ app/controllers/FrameworkController.java
@@ -20,57 +20,75 @@ public class FrameworkController extends Controller {
     Path currentRelativePath = Paths.get("");
     String strFilePath = currentRelativePath.toAbsolutePath().toString();
     public static File xlsfile;
-    public CompletionStage<Result> createExcel() throws  Exception{
-       System.out.println(request());
-        System.out.println(request().body().asJson().toString());
-        String strFrameworkId = request().body().asJson().get("framework").asText();
-        FWCreateExcel fwCreateExcel = new FWCreateExcel();
-       Map<String,Object> res = fwCreateExcel.createExcel(strFrameworkId);
-       File data= ((File)res.get("file"));
-       Result result = ok(data);
-CompletionStage<Result> response =
-        CompletableFuture.completedFuture(result);
-return response;
+    public CompletionStage<Result> createExcel(){
+        CompletionStage<Result> response = null;
+        try {
+            String strFrameworkId = request().body().asJson().get("framework").asText();
+            FWCreateExcel fwCreateExcel = new FWCreateExcel();
+            Map<String, Object> res = fwCreateExcel.createExcel(strFrameworkId);
+            File data = ((File) res.get("file"));
+            Result result = ok(data);
+             response =
+                    CompletableFuture.completedFuture(result);
+            return response;
+        }
+        catch(Exception e){
+            System.out.println(e);
+            return response;
+        }
     }
-    public CompletionStage<Result> publishFramework() throws  Exception{
-        String strFrameworkId = request().body().asJson().get("framework").asText();
-        String strChannel = request().body().asJson().get("channel").asText();
-        IniFile loadConfig = new IniFile(strFilePath + "/configLive.ini");
+    public CompletionStage<Result> publishFramework(){
+        CompletionStage<Result> response = null;
+        try {
+            String strFrameworkId = request().body().asJson().get("framework").asText();
+            String strChannel = request().body().asJson().get("channel").asText();
+            IniFile loadConfig = new IniFile(strFilePath + "/configLive.ini");
+            FWNewMasterFile fwNewMasterFile = new FWNewMasterFile(loadConfig);
+            String data =  fwNewMasterFile.publishFramework(strFrameworkId,strChannel);
+            Result result = ok(data);
+            response = CompletableFuture.completedFuture(result);
+            return response;
+        }
+        catch (Exception e){
+            System.out.println(e);
+            return response;
+        }
 
-        FWNewMasterFile fwNewMasterFile = new FWNewMasterFile(loadConfig);
-      String data =  fwNewMasterFile.publishFramework(strFrameworkId,strChannel);
-        Result result = ok(data);
-        CompletionStage<Result> response =
-                CompletableFuture.completedFuture(result);
-        return response;
     }
     public CompletionStage<Result> createupdateoperation() throws  Exception{
         int opt =1;
-        Http.MultipartFormData body = request().body().asMultipartFormData();
-        Http.MultipartFormData.FilePart<File> filePart = body.getFile("File");
-        String strFileExtn = (filePart.getFilename().substring(filePart.getFilename().lastIndexOf(".")+1));
+        String strFrameworkDescr = "";
+        CompletionStage<Result> response = null;
+        try {
+            Http.MultipartFormData body = request().body().asMultipartFormData();
+            Http.MultipartFormData.FilePart<File> filePart = body.getFile("File");
+            String strFileExtn = (filePart.getFilename().substring(filePart.getFilename().lastIndexOf(".") + 1));
+            xlsfile = filePart.getFile();
+            String strFrameworkName = (((String[]) (body.asFormUrlEncoded().get("fwName")))[0]);
+            String strFrameworkId = (((String[]) (body.asFormUrlEncoded().get("fwCode")))[0]);
+            if (body.asFormUrlEncoded().get("fwDescription") != null) {
+                strFrameworkDescr = (((String[]) (body.asFormUrlEncoded().get("fwDescription")))[0]);
 
-         xlsfile = filePart.getFile();
-        String strFrameworkName =  (((String[]) (body.asFormUrlEncoded().get("fwName")))[0]);
-        String strFrameworkId = (((String[]) (body.asFormUrlEncoded().get("fwCode")))[0]);
-        String strFrameworkDescr = (((String[]) (body.asFormUrlEncoded().get("fwDescription")))[0]);
-        String action = (((String[]) (body.asFormUrlEncoded().get("action")))[0]);
-
-        String strChannel = (((String[]) (body.asFormUrlEncoded().get("channel")))[0]);
-        IniFile loadConfig = new IniFile(strFilePath + "/configLive.ini");
-        if(action.equalsIgnoreCase("update")){
-            opt = 2;
             }
-        else if(action.equalsIgnoreCase("create")){
-            opt = 1;
-        }
-        FWNewMasterFile fwNewMasterFile = new FWNewMasterFile(loadConfig);
-        String data =  fwNewMasterFile.createFramework(xlsfile, strFileExtn,strFrameworkName, strFrameworkId, strFrameworkDescr, strChannel,opt);
+            String action = (((String[]) (body.asFormUrlEncoded().get("action")))[0]);
+            String strChannel = (((String[]) (body.asFormUrlEncoded().get("channel")))[0]);
+            IniFile loadConfig = new IniFile(strFilePath + "/configLive.ini");
+            if (action.equalsIgnoreCase("update")) {
+                opt = 2;
+            } else if (action.equalsIgnoreCase("create")) {
+                opt = 1;
+            }
+            FWNewMasterFile fwNewMasterFile = new FWNewMasterFile(loadConfig);
+            String data = fwNewMasterFile.createFramework(xlsfile, strFileExtn, strFrameworkName, strFrameworkId, strFrameworkDescr, strChannel, opt);
 
-        Result result = ok(data);
-        CompletionStage<Result> response =
-                CompletableFuture.completedFuture(result);
-        return response;
+            Result result = ok(data);
+            response = CompletableFuture.completedFuture(result);
+            return response;
+        }
+        catch (Exception e){
+            System.out.println(e);
+            return response;
+        }
     }
 
 }
